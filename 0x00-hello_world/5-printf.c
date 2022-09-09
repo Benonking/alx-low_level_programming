2f835400a19b134025937612b21bb2a9d33c8bd8
Benonking <benonkighom@gmail.com>
2022-09-08 22:06:55 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 /*
- *
  *prints text to standard out put 
- *
  * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
-
 }
