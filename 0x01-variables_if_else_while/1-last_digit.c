794da7ec0187bc32b2f865d6750206106b8c3718
Benonking <benonkighom@gmail.com>
2022-09-11 03:41:22 -0700

    domit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	printf("Last digit of %i is %i and is ", n, (n % 10));
+	printf("Last digit of %i is %i ", n, (n % 10));
 
 	if ((n % 10) == 0)
 	{
