ddb77f504d21afbaee5b1758f78c34525cbb9738
Benonking <benonkighom@gmail.com>
2022-09-12 06:55:13 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ * Return \: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int tens;
+	int ones;
+	int x;
+	int o;
+
+	for (tens = '0'; tens <= '9'; tens++)/*print two digit cobination*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (x = tens; x <= '9'; x++)
+			{
+				for (o = ones + 1; o <= '9'; o++)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(' ');
+					putchar(x);
+					putchar(o);
+
+					if (!((tens == '9' && ones == '8') &&
+								(x == '9' && o == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				o = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
