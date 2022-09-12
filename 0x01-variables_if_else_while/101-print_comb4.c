d901142413466bde18876f956716599772cf2e05
Benonking <benonkighom@gmail.com>
2022-09-12 06:17:15 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int hundreds;
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = (hundreds + 1); tens <= '9'; tens++)
+		{
+			for (ones = (tens + 1); ones <= '9'; ones++)
+			{
+				putchar(ones);
+				putchar(tens);
+				putchar(hundreds);
+				if (hundreds != '7' || tens != '8' || ones != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
