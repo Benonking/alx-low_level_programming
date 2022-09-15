ee381a2d54ffbf44e0c44b466b264f108594f69c
Benonking <benonkighom@gmail.com>
2022-09-15 11:23:30 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,7 +6,7 @@
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')i
 	{
 		return (1);
 	}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first argument to function
+ * @b: second argument to fnction
+ * Retun: multiple
+ */
+
+int mul(int a, int b)
+{
+	mul = a * b;
+
+	return (mul);
+}
