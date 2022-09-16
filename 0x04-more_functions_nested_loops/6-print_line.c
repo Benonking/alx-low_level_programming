d69c416398885370f5dab52ce812da5fcf8ee856
Benonking <benonkighom@gmail.com>
2022-09-15 20:04:04 -0700

    Draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line- Print a staignt line
+ * @n:argument of the function
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	for (i = n; i < n; n--)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
