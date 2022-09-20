c615d7ec6b9bc3734efd28411304e2c5fb202311
Benonking <benonkghoma@gmail.com>
2022-09-20 12:18:03 -0700

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,15 +8,13 @@
 
 int _strlen(char *s)
 {
-        int l = 0;
-
-        while (*s != '\0')
-        {
-                l++;
-                s++;
-        }
-
-        return (l);
+	int l = 0;
+	while (*s != '\0')
+	{
+		l++;
+		s++;
+	}
+	return (l);
 }
 
 /**
