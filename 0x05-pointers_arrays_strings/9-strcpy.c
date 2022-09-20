6d9f266085b639e7fee0d0469cba454f1303a7a3
Benonking <benonkghoma@gmail.com>
2022-09-20 05:03:54 -0700

    copy string to buffer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * *_strcpy- copy string pointed to src to buffer pointed to dest
+ * @src: source
+ * @dest:distination buffer
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len_src = _strlen(str);
+	int i;
+
+	for (i = 0; i <= len_src; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
