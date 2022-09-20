fd833735845db7f8479bdbaa78cc08ff171eb1aa
Benonking <benonkghoma@gmail.com>
2022-09-20 11:36:40 -0700

    Convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,30 +7,5 @@
 
 int _atoi(char *s)
 {
-	unsigned int tens, digit, positive;
-	double t_beg = 1;
-
-	if (positive == 0)
-		_putchar('0');
-	else
-	{
-		if (positive < 0)
-		{
-			positive = positive * -1;
-			_putchar('-');
-		}
-
-		while (t_beg <= positive)
-			t_beg *= 10;
-		tens = t_beg / 10;
-
-		while (tens >= 1)
-		{
-			digit = positive / tens;
-			_putchar(digit + '0');
-			positive = (positive - (tens * digit));
-			tens /= 10;
-		}
-	}
 }
 
