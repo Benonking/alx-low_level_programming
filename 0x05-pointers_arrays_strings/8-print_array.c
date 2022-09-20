34db18442b785e9fa17c30e5ca51c404c4b3c032
Benonking <benonkghoma@gmail.com>
2022-09-20 04:21:27 -0700

    print n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
+		if (i != (n-1))
+		{
+			_putchar(a[i]);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else 
+			_putchar(a[i]);
 	}
 	_putchar('\n');
 }
