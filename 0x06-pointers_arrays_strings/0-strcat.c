a9216a44c630828db8b88e6eb918e98d2b535ffe
Benonking <benonkghoma@gmail.com>
2022-09-21 11:44:03 -0700

    concat strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcat- appends a string src to the end of another string dest
+ * @src: original string
+ * @dest: destination string
+ * Return: Dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0')/* find end  of src */
+		i++;
+	while ((dest[i++] = src[j++]) != '\0')/*copt src*/
+		;
+}
