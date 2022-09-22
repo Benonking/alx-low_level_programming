f7287470ec454e4721fcc635ce31ca4216c46bfb
Benonking <benonkghoma@gmail.com>
2022-09-22 06:17:44 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,12 +3,13 @@
 /**
  * reverse_array - reverse array
  * @a: array
- * @n: number of elements to reverse arrays
+ * @n: number of elements in array
  * Return: Always 0
  */
 
 void reverse_array(int *a, int n)
 {
+
 	int tmp, beg = 0;/*begin of array*/
 	int end = n - 1;/*end of array*/
 
