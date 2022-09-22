3d8c61a89c96591c586ed7dfeaae564fbb14cda0
Benonking <benonkghoma@gmail.com>
2022-09-22 11:50:49 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
 
-	for (i = 0; s1[i] != '\0'; i++)
 		s1++;
-	for (j = 0; s2[j] != '\0'; j++)
 		s2++;
-
-	return (s1++ - s2++);
-}
+	}
+	return (0)
