64ed685aebd9657b43c4a4fffb23817ef550477c
Benonking <benonkghoma@gmail.com>
2022-09-22 04:48:36 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - cmpaore two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: return int 
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	for (i = 0; s1[i] != '\0'; i++)
+		s1++;
+	for (j = 0; s2[j] != '\0'; j++ )
+		s2++;
+
+	return (s1++ - s2++);
+}
