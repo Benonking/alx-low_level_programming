373d572ec5237ee2355ac22a9e2a384b4e314eb6
Benonking <benonkghoma@gmail.com>
2022-09-22 11:07:02 -0700

    copy n characters to empty buffer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,14 +7,18 @@
  * @n: n characters to be copied
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		if (src[i] < src[n])
-			dest[i] = src[i];
+		dest[i] = src[i];
+	while (i < n)
+	{
 		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
