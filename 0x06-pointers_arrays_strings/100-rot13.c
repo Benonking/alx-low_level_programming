785b030be297846fb902c0e7c35c4142dabb33d8
Benonking <benonkghoma@gmail.com>
2022-09-22 23:45:20 -0700

    Encode string to rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string to rot13
+ * @s: string 
+ * Return: rot13 string
+ */
+
+char *rot13(char *s)
+{
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+
+	int j, i;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+		if (s[j] == a[i])
+			s[j] = b[i];
+	}
+
+	return (s);
+}
