5e90130919612dda0459e7e971985210b6bfdcc2
Benonking <benonkghoma@gmail.com>
2022-09-27 01:26:21 -0700

    copy n bytes of memory from source to destination

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,12 +10,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	int size = n
 
-	if (n > 0)
+	if (size > 0)
 	{
 		unsigned int i;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 			*(dest + i) = *(src + i);
 	}
 
