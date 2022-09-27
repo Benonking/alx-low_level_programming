d2f156051e437b6b0f3e0c5618274e0a8535ec85
Benonking <benonkghoma@gmail.com>
2022-09-27 01:59:13 -0700

    locate 1st occurrence of char in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-			return (c);
-		else
-			return (NULL);
-	}
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	if (s[i] == c)
+		return (&s[i]);
+	else
+		return (NULL);
+	
 }
