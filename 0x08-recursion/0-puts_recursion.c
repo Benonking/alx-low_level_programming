e2eef86c630b7478153a92ca8fb0cd1bc8c2c48a
Benonking <benonkghoma@gmail.com>
2022-09-28 07:52:09 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
 		return;
 	}
-	s++;
-	_puts_recursion(s);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	_putchar('\n');
 }
