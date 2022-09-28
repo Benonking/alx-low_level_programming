565650bdbca39990b42428fd2f2d0b1729006e27
Benonking <benonkghoma@gmail.com>
2022-09-28 02:50:43 -0700

    print string

---
+++ 0x08-recusion/0-puts_recusion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		_putchar(s);
+		_putchar('\n');
+	}
+
+	return (0);
+}
