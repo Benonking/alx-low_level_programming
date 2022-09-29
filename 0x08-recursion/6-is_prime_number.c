81374429da812163c07e8fd3c312d42acd246e62
Benonking <benonkghoma@gmail.com>
2022-09-29 02:14:43 -0700

    prime number using recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int primeNUmber(int p, int divider);
+/**
+ * is_prime_number - return 1 if n is a prime number else return 0
+ * @n: check integer
+ * Return: 1 0r 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (primeNumber(n, n - 1));
+}
+
+/**
+ * primeNumber - return prime numner
+ * @p: number yo evaluate
+ * @div: divider
+ * Return: 1 if p is prime 0 if not
+ */
+
+int primeNumber(int p, int div)
+{
+	if (div == 1)
+		return (1);
+
+	if (n % div == 0 && div > 0)
+		return (0)
+
+
+		return (primeNumber(p, div - 1));
+}
