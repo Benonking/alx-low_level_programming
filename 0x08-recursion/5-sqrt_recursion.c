39e327f865e551cdf287f0bfbda1067ae52e0fbb
Benonking <benonkghoma@gmail.com>
2022-09-29 00:33:32 -0700

    print sqaure root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int sqr_recursion(int n, int i);
+/**
+ * _sqrt_recursion - print natural  sqaure root of n
+ * @n: number
+ * Return: xsquare root of n
+ */
+int _sqrt_recusion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqr_recursion(n, 0));
+}
+/**
+ * sqr_recursion - sqaurea number
+ * @n: square of a number
+ * @i: square root of n
+ * Return: sqaure
+ */
+
+int sqr_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqr_recursion(n, i + 1));
+}
