10570d95c71cd6e32be3011893242a3f68b5ea20
Benonking <benonkghoma@gmail.com>
2022-09-30 02:41:07 -0700

    print number og arguments of main

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print number of argumets
+ * @argc: number of arguments
+ * @argv: pointer array to string arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (*argv[i] > 0)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
