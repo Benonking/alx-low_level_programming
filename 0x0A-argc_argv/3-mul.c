52b6f4b1cc4fb7aced03d4f38f797895287d4c64
Benonking <benonkghoma@gmail.com>
2022-09-30 04:05:08 -0700

    multiply arguments of a function

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two integers arguments
+ * @argc: number of arguments
+ * @argv: pointer to array containing arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
