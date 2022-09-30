259774d5db873a9511433d7fe285e91f4641f944
Benonking <benonkghoma@gmail.com>
2022-09-30 04:57:40 -0700

    print number og arguments of main

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	if (argc > 0)
 		printf("%d\n", argc - 1);
