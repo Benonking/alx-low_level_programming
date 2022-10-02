82f6e4b9b8dcc19c03ee3628463eb5f89b31deda
Benonking <benonkghoma@gmail.com>
2022-10-02 08:55:48 -0700

    static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,13 +1,7 @@
 #include "main.h"
 
-/**
- * main - print text
- * Return: Always 0
- */
-
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best
-	hacker\"\n\t- Kevin Mitnick");
-	return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
