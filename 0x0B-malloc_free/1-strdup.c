4a24edbc2b7a6fc33400be643f8ae8022871f20f
Benonking <benonkghoma@gmail.com>
2022-10-04 23:04:25 -0700

    new allocation in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,34 @@
 #include "main.h"
-#define NULL 0
 #include <stdlib.h>
 
 /**
- * _strdup - return pointer to a newly  allocated space in mem
- * @str: pointer to string paramenter
- * Return: pointer to string
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ * containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ *
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * Otherwise - a pointer to the duplicated string.
  */
-
 char *_strdup(char *str)
 {
-	char *duplicate_str;
-	int i = 0;
+	char *duplicate;
+	int index, len = 0;
 
-	if (str == 0) /* validate str input */
-	if (str == 0) /* validate str input */
+	if (str == NULL)
 		return (NULL);
-	duplicate_str = malloc(sizeof(str)); /* allocate memory */
 
-	if (duplicate_str == NULL) /* validate memory */
+	for (index = 0; str[index]; index++)
+		len++;
+
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
 		return (NULL);
-	while (*(str + i))
-	{
-		*(duplicate_str + i) = *(str + i);
-		i++;
-	}
-	*(duplicate_str + i) = '\0';
-	return (duplicate_str);
+
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
+
+	duplicate[len] = '\0';
+
+	return (duplicate);
 }
