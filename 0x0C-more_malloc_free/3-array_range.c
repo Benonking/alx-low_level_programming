5866042c35adc28ad58edeec399c97ebd96bad04
Benonking <benonkghoma@gmail.com>
2022-10-07 08:41:13 -0700

    allocate men to a range of vals in an array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - create an array of integers
+ * @min: min of range
+ * @max: max of range
+ * Return: pointer to newly allocated array*/
+int *array_range(int min, int max)
+{
+	void *a;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	a =  malloc(sizeof(int) * (max - min + 1));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < (max - min + 1); i++)
+		a[i] = min++;
+
+	return (a);
+}
