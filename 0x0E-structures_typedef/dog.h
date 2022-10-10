f7a543429bc9d0c8ea8a7faed12d5d2fd8f08a26
Benonking <benonkghoma@gmail.com>
2022-10-10 06:51:49 -0700

    define struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog name, age, owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+define struct dog
+#endif
