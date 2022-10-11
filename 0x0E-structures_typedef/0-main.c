b43f5a9afa817a462a8620d546b4cb06244a4af0
Benonking <benonkghoma@gmail.com>
2022-10-11 01:14:40 -0700

    define dog attributes

---
+++ 0x0E-structures_typedef/0-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check code
+ *
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = "3.5";
+	my_dog.owner = "Bob";
+
+	printf("My name is %s, and I am %.lf :) - Woof!\n", my_dog.age);
+
+	return (0);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - dog name, age, owner
  * @name: name of dog
@@ -7,9 +7,17 @@
  * @owner: owner of dog
  */
 
-typedef struct dog {
+struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
