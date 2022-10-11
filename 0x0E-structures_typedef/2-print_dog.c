45fb94a3c8ec7dfbfef1b36aee22818a10a1e0b3
Benonking <benonkghoma@gmail.com>
2022-10-11 01:46:56 -0700

    pritn struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+
+/**
+ * print_dog - print struct dog
+ * @d: pointer to struct dog
+ * Return: Always 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	{
+		return;
+	}
+	else
+	{
+		if (d->name == NULL)
+			print("Name: (nil)\n");
+		else
+			 print("Name: %s\n", d->name);
+
+		 print("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			print("Owner: (nil)\n");
+		else
+			print("Owner: %s\n", d->owner);
+	}
+}
