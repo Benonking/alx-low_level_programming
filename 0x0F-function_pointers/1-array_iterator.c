1027afb74389c179e636c9463fd0d06f1ae5e3af
Benonking <benonkghoma@gmail.com>
2022-10-12 06:31:28 -0700

    execute function stored in an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,13 +4,13 @@
  * array_iterator - execute funcations given as paramaner in an array
  * @array: pointer to array
  * @size: size of the array
- * @action: pointer to function 
+ * @action: pointer to function
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	if (array != NULL && action != NULL)
-		for (i = 0; array[i] < size; i ++)
+		for (i = 0; array[i] < size; i++)
 		{
 		action(array[i]);
 		}
