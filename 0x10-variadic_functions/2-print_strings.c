ecd5d6e593c68b63863757c3c73fdb35cf98191b
Benonking <benonkghoma@gmail.com>
2022-10-14 03:38:03 -0700

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print numbers
+ * @separator: separotor of nmumbers
+ * @n: number of arguments
+ *
+ * Return: Always 0
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list pa;
+
+	if (n > 0)
+	{
+		va_start(pa, n);
+		for (i = 0; i < n; i++)
+		{
+		if (va_arg(pa, char *) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(pa, char));
+		if (i != n && separator != NULL)
+			printf("%s", separator);
+		}
+		va_end(pa);
+	}
+	printf("\n");
+}
+
