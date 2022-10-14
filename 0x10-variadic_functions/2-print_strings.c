ff8896b36b0b671095f000e8c1d23a1df840fdfc
Benonking <benonkghoma@gmail.com>
2022-10-14 13:53:46 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,7 +25,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("(nil)");
 			else
 				printf("%s", s);
-			
 			if (i != n && separator != NULL)
 				printf("%s", separator);
 		}
