4c8f0416b4947b031297d36749d55905ad93b2d9
Benonking <benonkghoma@gmail.com>
2022-10-21 11:31:35 -0700

    free linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list
+ * @head: pointer to head of list
+ * Return: Always 0
+ *
+ */
+
+void free_list(list_t *head)
+{
+	if (*head == NULL)
+		return (NULL);
+	free(head->str);
+	free(head);
+	head = head->next;
+	return (0);
+}
