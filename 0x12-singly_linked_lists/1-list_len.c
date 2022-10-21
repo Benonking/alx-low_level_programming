233337d87acf6c29fe8800dc710fca81190725d3
Benonking <benonkghoma@gmail.com>
2022-10-21 02:49:54 -0700

    return numner of elemets in a liked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - return the length of a list
+ * @h: point elemtent in list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
