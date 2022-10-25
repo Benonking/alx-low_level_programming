7982c1f9dca589d23628d97265933435d65b8652
Benonking <benonkghoma@gmail.com>
2022-10-25 07:08:08 -0700

    print sum of data nodes

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_listint - return the sum of all the data in list
+ * @head: head of the list
+ * Return: sum 
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head->next != NULL)
+		sum += head->n;
+	return (sum);
+}
