e21a95cc482314749d576383667d588ebbbf740b
Benonking <benonkghoma@gmail.com>
2022-10-25 05:58:54 -0700

    delete head of list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head of linked list
+ * @head: pointer to head of list
+ * Return: Data of head
+ */
+
+int pop_listint(listint_t **head)
+{
+	int data;
+	listint_t *tmp;/*create a temp node*/
+
+	if (*head == NULL)
+		return (0);
+	tmp = *head;
+
+	data  = tmp->n;/*save data to return later*/
+	*head = tmp->next;/*link head to next node*/
+	free(tmp);
+}
+
