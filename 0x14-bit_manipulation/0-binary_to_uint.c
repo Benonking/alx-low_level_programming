370163cc2087651237064fc8efd365b7fac7c299
Benonking <benonkghoma@gmail.com>
2022-10-28 03:35:57 -0700

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,24 +6,29 @@
  * Return: integer or 0 if there is char that isnt (0,1)
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int len = 0;
-	unsigned int sum = 0,
+	unsigned int sum = 0;
 	int i;
 
 	if (b == NULL)
 		return (0);
 
 	while (b[len] != '\0')
+	{
 		len++;
+	}
 	len = len - 1;
+	i = 0;
 	while (b[i])
 	{
-	if (b[i] != '0' && b[i] != '1')
-		return (0);
-	if (b[i] == '1')
-		sum += (1 * (1 << len));
-	i++;
-	len--;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
+}
+	return (sum);
 }
