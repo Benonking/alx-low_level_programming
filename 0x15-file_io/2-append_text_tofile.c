97294e4558b9e19381a8c8b5f0c12d53060b00e6
Benonking <benonkghoma@gmail.com>
2022-11-01 06:46:52 -0700

    append at end opf file

--- 0x15-file_io/2-append_text_tofile.c
+++ 0x15-file_io/2-append_text_tofile.c
@@ -1,54 +1,32 @@
 #include "main.h"
 
 /**
- * _strlen - find length of string
- * @str: string
- * Return: length
- */
-int _strlen(char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-
-	return (len);
-}
-
-/**
- * append_text_to_file - append text and only if file exists
- * @filename: file
- * @text_content: appends this content into file
- * Return: 1 on success, -1 on error
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file.
+ * @text_content: String to append at the end of a file.
+ * Return: 1 if the file exist, -1 if the file does not exist, or if you
+ * don't have the required permissions to write to the file.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int n_wrote;
+	int i;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-
-	/* open file if it exists */
-	fd = open(filename, O_WRONLY | O_APPEND);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-
-	/* if nothing to write, still successful */
-	if (!text_content)
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
-		close(fd);
-		return (1);
+		if (write(fd, &text_content[i], 1) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-
-	/* write */
-	n_wrote = write(fd, text_content, _strlen(text_content));
-	if (n_wrote == -1 || n_wrote != _strlen(text_content))
-	{
-		close(fd);
-		return (-1);
-	}
-
 	close(fd);
 	return (1);
 }
