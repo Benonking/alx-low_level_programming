e532d0cffbba93207b774836b08b4db331a184d6
Benonking <benonkghoma@gmail.com>
2022-11-01 06:55:09 -0700

    read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,50 @@
 #include "main.h"
 
 /**
- * read_textfile - read text file and print it to stdout
- * @filename: filename
- * @letters: number of leters to read and print
- *
- * Return: 0 if file cant be read or NULL else pritnc contents
+ * read_textfile - Reads a file and prints it to the POSIX stdout.
+ * @filename: The name of the file that needs to be read.
+ * @letters: Is the number of letters the function should print.
+ * Return: The actual number of letters it could read and print.
  */
 
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;/*file discriptor*/
-	size_t readd, writee;
-	char *buffer;
+	int fd;
+	int i;
+	int read_letters;
+	char *buf;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);/*open file*/
-	/*check if file can be opened*/
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-		return (0);
-
-	readd = read(fd, buffer, letters);
-	if (readd == -1)
 	{
-		free(buffer);
-		close(fd);
+		free(buf);
 		return (0);
 	}
 
-	/*write*/
-
-	writtee = write(STDOUT_FILENO, buffer, readd);
-	if (writtee == -1)
+	read_letters = read(fd, buf, letters);
+	if (read_letters == -1)
 	{
-		free(buffer);
 		close(fd);
+		free(buf);
 		return (0);
+	}
+
+	for (i = 0; i < read_letters; i++)
+	{
+		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
+		{
+			close(fd);
+			free(buf);
+			return (0);
+		}
+
 	}
 	close(fd);
-	return (readd);
+	free(buf);
+	return (read_letters);
 }
