850303b93eb904df0da86c3c8125b1ac62c5c257
Benonking <benonkghoma@gmail.com>
2022-12-08 01:17:26 -0800

    len of doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+ * dlistint_len - return lengtrh of doubly linked list
+ * @h: point to head of list
+ * Return: length of list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	i = 0;
+	while(h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
