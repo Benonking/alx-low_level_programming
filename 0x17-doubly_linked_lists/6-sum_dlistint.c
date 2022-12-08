6e0cdc06c040e60ce3fd8203d0bdcd8f0519312a
Benonking <benonkghoma@gmail.com>
2022-12-08 06:51:48 -0800

    add elements of doubly liked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,12 +3,13 @@
 /**
  * sum_dlistint - sum all elements of list
  * @head: point to head of list
- * Return: 0 if no list or sum 
+ * Return: 0 if no list or sum
  */
 
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
+
 	if (!head)
 		return (0);
 	while (head != NULL)
@@ -16,5 +17,5 @@ int sum_dlistint(dlistint_t *head)
 		sum += head->n;
 		head = head->next;
 	}
-	return sum;
+	return (sum);
 }
