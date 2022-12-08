859063cecb1b6ac532e67e1edd3d508d79ddf891
Benonking <benonkghoma@gmail.com>
2022-12-08 01:05:36 -0800

    print doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print list nodes
+ * @h: point to head of list
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+	while (h != NULL)
+	{
+		h = n->next;
+		i++;
+	}
+	return(i);
+}
