133583de119fca76a494e10648f15f26b61bcc3c
Benonking <benonkghoma@gmail.com>
2022-12-08 06:00:56 -0800

    return node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get a node at a specific index
+ * @head: point to head of list
+ * @index: position of node
+ * Return: node
+ *
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	if (!head)
+		return (NULL);
+	while (head != NULL)
+	{
+		head = head->next;
+		i++;
+		if (i == index)
+			return (head);
+	}
+	if (index > i)
+		return (NULL);
+}
