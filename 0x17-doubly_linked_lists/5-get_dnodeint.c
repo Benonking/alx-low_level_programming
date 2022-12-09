1034a5fa6893288d1c1ab23a45e3ae5462586fdb
Benonking <benonkighoma@gmail.com>
2022-12-09 06:53:23 -0800

    get element at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,8 +16,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		head = head->next;
 		index -= 1;
+		if (index == 0)
+			return (head);
 	}
-	if (index == 0)
-		return (head);
 	return (NULL);
 }
