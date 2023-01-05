c64ade5919f422fa5bc979e2582f2d5d18a9f69d
Benonking <benonkighoma@gmail.com>
2023-01-05 11:44:28 -0800

    delete extras

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - multiply to integers
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0 always
+ */
+
+int main (int argc, char **argv)
+{
+	int i;
+
+	if (argc > 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; argv[i]; i++)
+		
+		num = atoi(argv[i])
+}
+
---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *_strcpy- copy string pointed to src to buffer pointed to dest
+ * @src: source
+ * @dest:distination buffer
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
