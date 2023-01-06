0f4ab6344faad9a8a76305cc257105874d7e9ef1
Benonking <benonkighoma@gmail.com>
2023-01-06 10:47:29 -0800

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,27 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i;
-	/*no table*/
-	if (size == 0)
-		return (NULL);
-
+	unsigned int i = 0;
 	hash_table_t *table;
-	table = malloc(sizeof(hash_table_t));
 	hash_node_t **array;
-	array = malloc(sizeof(hash_node_t) * size);
+	/*no table*/
+        if (size == 0)
+		return (NULL);
 
+	
+	table = malloc(sizeof(hash_table_t));
 	/*fail*/
-	if (table == NULL)
-		return (NULL);
+        if (table == NULL)
+                return (NULL);
 
+	array = malloc(sizeof(hash_node_t) * size);
+	/*fail*/
 	if (array == NULL)
+	{
+		free(table);
 		return (NULL);
-
+	}
+	/*initialize array elements to NULL*/
 	while (i < size)
 	{
 		array[i] = NULL;
