bc630d123eb62a4fda6667c62204f6de2c562199
Benonking <benonkighoma@gmail.com>
2023-01-06 06:18:32 -0800

    header file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * hash_table_create - create a hash table
+ * @size: size of hash table
+ * Return: hash table
+ *
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
