822d09a6193df0ecd8381e3c40f2f6cdf607d3c7
Benonking <benonkighoma@gmail.com>
2023-01-31 10:56:34 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen- return length of string
+ * @s: string
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while(*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
